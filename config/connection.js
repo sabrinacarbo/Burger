4c4a85f25facf1c0c1ebf1ddd44b0ff471e7b62a
Sabrina Carbo <sabrinacarbo@Sabrinas-MacBook-Pro.local>
2018-08-02 16:31:06 -0400

    heroku deployment

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,22 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-    "host": "localhost",
-    "user": "root",
-    "password": "root",
-    "database": "burgers_db",
-    "port": 8889
-});
+var connection;
 
-connection.connect (function(err){
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        "host": "localhost",
+        "user": "root",
+        "password": "root",
+        "database": "burgers_db",
+        "port": 8889
+    });
+};
 
-    if (err){
+connection.connect(function (err) {
+
+    if (err) {
         console.error("error connecting " + err.stack);
         return;
     }
