7f6be497c038581d33b47913c5065c84e542bed1
Sabrina Carbo <sabrinacarbo@Sabrinas-MacBook-Pro.local>
2018-08-02 20:00:48 -0400

    /api/burgers working

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -29,13 +29,22 @@ router.post("/api/burgers", function(req, res) {
   });
 });
 
+router.get("/api/burgers", function(req, res) {
+  burger.all(function(data) {
+    var burgerList = {
+      burgers: data
+    };
+    res.json(burgerList);
+  });  
+});
+
 router.put("/api/burgers/:id", function(req, res) {
   var condition = "id = " + req.params.id;
 
   console.log("condition", condition);
 
   burger.update({
-    sleepy: req.body.devoured
+    devoured: req.body.devoured
   }, condition, function(result) {
     if (result.changedRows == 0) {
       // If no rows were changed, then the ID must not exist, so 404
